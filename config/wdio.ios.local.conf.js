ca1a4d62537741f9e4013a24d13164eb236daf6b
Reinaldo <reinaldo.rossetti@outlook.com>
2026-08-11 17:03:03 -0300

    tentando arrumar a esteira do IOS, que ainda não estava concluida, analisando erros de timeout

--- config/wdio.ios.local.conf.js
+++ config/wdio.ios.local.conf.js
@@ -8,6 +8,8 @@ const __dirname = path.dirname(__filename)
 export const config = {
   ...sharedConfig,
   port: 4723,
+  maxInstances: 1,
+  maxInstancesPerCapability: 1,
   services: [
     ['appium', {
       logPath: path.resolve(__dirname, '../logs')
@@ -31,7 +33,7 @@ export const config = {
     'appium:deviceName': process.env.IOS_DEVICE_NAME || 'iPhone 15',
     'appium:platformVersion': process.env.IOS_PLATFORM_VERSION || '17.5',
     'appium:automationName': 'XCUITest',
-    'appium:app': path.resolve(__dirname, '../app/ios/ios.simulator.wdio.native.app.v2.2.0.zip'),
+    'appium:app': path.resolve(__dirname, '../app/ios/ios.simulator.wdio.native.app'),
     'appium:wdaLaunchTimeout': 180000,     // Tempo limite para o WebDriverAgent iniciar (3 min)
     'appium:wdaConnectionTimeout': 180000, // Tempo limite de conexão com o WDA
     'appium:commandTimeouts': '120000',    // Tempo limite para comandos gerais
